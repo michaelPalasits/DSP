3487a5f6464fe51318cc6b9f4365a6f6658e28e7
michaelPalasits <47963891+michaelPalasits@users.noreply.github.com>
2019-07-03 16:54:17 -0600

    stage 1 draft done
    works for n=4,8. Doesn't work for n = oth.

--- radix2_fft_iterative.m
+++ radix2_fft_iterative.m
@@ -5,18 +5,17 @@ clc
 x = 0:1:7;
 N = length(x);
 stages =  log2(N);
-xCol = [x(1:N/2); x(N/2+1:N)]';
-% xCol = split(x,N);
 xprime = x;%copy of input for comparison
 
 %% stage 1
-% compute DFTs
-% even branch
-xn(1:2) = dft(xCol(1,:),N/4);
-xn(3:4) = dft(xCol(3,:),N/4);
-% odd branch
-xn(5:6) = dft(xCol(2,:),N/4);
-xn(7:8) = dft(xCol(4,:),N/4);
+WN = exp(-1i*2*pi/2);
+k = 0:1;
+for i = 1:N/4
+    xn(2*i-1:2*i) = x(2*i-1)+x(2.^(stages-1)+2*i-1).*WN.^k;
+end
+for i = N/4+1:N/2
+    xn(2*i-1:2*i) = x(2*i-2^(stages-1))+x(2*i).*WN.^k;
+end
 x = xn;
 %% all other stages
 for i = 2:stages
@@ -31,14 +30,4 @@ for i = 2:stages
         end
     end
     x = xn;
-end
-
-function xColWise = split(x,N)
-
-xColWise = zeros(1,N);
-for i = 1:N/2
-    xColWise(2*i-1) = x(i);
-    xColWise(2*i) = x(i+N/2);
-end
-
 end
